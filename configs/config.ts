99c01eb5059ea257c2f3a95f3cd65cad36815c09
dannysofftie <dankim761@gmail.com>
2018-09-25 16:37:12 +0300

    Add MySQL configs

---
+++ configs/config.ts
@@ -0,0 +1,15 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+export function mysqlConfig(): string {
+    return `mysql://${process.env.MYSQL_USERNAME}:${process.env.MYSQL_PASSOWRD}@${process.env.MYSQL_HOST}:3306/${
+        process.env.MYSQL_DATABASE
+    }?connectionLimit=10&dateStrings=true`;
+}
+
+export function mongoConfig(): string {
+    return `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:27017/${
+        process.env.MONGO_DATABASE
+    }`;
+}
---
+++ configs/dbconfig.ts
@@ -0,0 +1,52 @@
+import { createPool, escape, Pool } from 'mysql';
+
+import { mysqlConfig } from './config';
+
+/**
+ * Database class to handle database queries
+ *
+ * @class Database
+ */
+class Database {
+    /**
+     * Database connection object
+     *
+     * @private
+     * @type {Pool}
+     * @memberof Database
+     */
+    private conn: Pool;
+
+    constructor() {
+        this.conn = createPool(mysqlConfig());
+    }
+
+    /**
+     * Execute passed sql into database
+     *
+     * @param {string} sql - sql query to execute
+     * @param {(Array<string | number> | any)} [params] - data to insert if any
+     * @returns {Promise<string[]>}
+     * @memberof Database
+     */
+    public query(sql: string, params?: Array<string | number> | any): Promise<object[]> {
+        return new Promise((resolve, reject) => {
+            typeof params === 'undefined'
+                ? this.conn.query(sql, (err, results) => (err ? reject(err) : resolve(results)))
+                : this.conn.query(sql, params, (err, results) => (err ? reject(err) : resolve(results)));
+        });
+    }
+
+    /**
+     * Escape passed value
+     *
+     * @param {string} param
+     * @returns {string}
+     * @memberof Database
+     */
+    public escape(param: string): string {
+        return escape(param);
+    }
+}
+
+export const database = new Database();
