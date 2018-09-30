283e2b787cd05093124aea6fc2cc52137c1ad41e
dannysofftie <dankim761@gmail.com>
2018-09-30 15:14:25 +0300

    Generate Mongo schema files

--- index.ts
+++ index.ts
@@ -1,13 +1,11 @@
 import { Migrate } from './src';
 
 const args = process.argv.slice(2);
-const options = {
-    '--database': '',
-    '--action': '',
-};
 
-(async () => {
+// invoke
+const d = (async () => {
     const migrate = new Migrate();
     await migrate.retrieveModels();
     await migrate.retrieveMysqlData();
+    await migrate.generateMongoSchemas();
 })();
