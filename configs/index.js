98c4647596be50d98de31922b088f4f9348b4413
Danny Sofftie <dankim761@gmail.com>
2019-01-03 16:03:18 +0300

    Remove unnecessary packages

--- configs/index.js
+++ configs/index.js
@@ -3,9 +3,6 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
 function mysqlConfig(options) {
     return `mysql://${options.mysqlusername.trim()}:${options.mysqlpassword.trim()}@${options.mysqlhost.trim()}:3306/${options.mysqldatabase.trim()}?connectionLimit=10&dateStrings=true`;
 }
